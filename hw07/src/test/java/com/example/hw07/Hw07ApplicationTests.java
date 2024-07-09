52b3b4d342eb9933bfb3a77fa38c043a88621803
Hokkey16 <louis7016@naver.com>
2024-07-09 19:06:14 +0900

    ver01

---
+++ hw07/src/test/java/com/example/hw07/Hw07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
